5ddd19af6af662466922ef4c00b8b51d5b365e81
Lee <gwforumlakers@gmail.com>
2020-06-11 16:03:56 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-var ionApp = angular.module('ionAppH', ['ionic', 'ngResource', 'mdo-angular-cryptography']);
+var ionApp = angular.module('ionAppH', [
+  'ionic',
+  'ngResource',
+  'mdo-angular-cryptography',
+]);
 
 ionApp.config(function(
   $stateProvider,
@@ -8,13 +12,13 @@ ionApp.config(function(
   $provide
 ) {
   $stateProvider
-    .state('eventmenu', {
-      url: '/event',
+    .state('app', {
+      url: '/app',
       abstract: true,
       templateUrl: 'htmplates/menu.html',
       controller: 'MainCtrl',
     })
-    .state('eventmenu.main', {
+    .state('app.main', {
       url: '/main',
       views: {
         menuContent: {
@@ -23,16 +27,16 @@ ionApp.config(function(
         },
       },
     })
-    .state('eventmenu.page1', {
-      url: '/page1',
+    .state('app.login', {
+      url: '/login',
       views: {
         menuContent: {
-          templateUrl: 'htmplates/page1.html',
+          templateUrl: 'htmplates/login.html',
           controller: 'MainCtrl',
         },
       },
     })
-    .state('eventmenu.details', {
+    .state('app.details', {
       url: '/details',
       views: {
         menuContent: {
@@ -42,7 +46,7 @@ ionApp.config(function(
       },
     });
 
-  $urlRouterProvider.otherwise('/event/main');
+  $urlRouterProvider.otherwise('/app/main');
 
   // gwfl:5821f61550e9b39131fe1b6f   rdb:569a2b87566759cf4b984a50
   $httpProvider.defaults.headers.common = {
@@ -54,121 +58,189 @@ ionApp.config(function(
   $provide.factory('bbCrypt', function() {
     return '123bb';
   });
-
-});  // end config
+}); // end config
 
 ionApp.run(function($rootScope, dbSvc) {
-  
-  $rootScope.userRec = {  _id: '',  utilkey: '',  
-    jsonData: {  usrnm: '',  accts: [ { xtra: '', pswd: '', acctnm: '' } ] } };
-  $rootScope.ngrAA = [];  $rootScope.ngrUU = [];
-  $rootScope.gwflRec = {users: [], appkey:'crypto', recID:''};
-  $rootScope.aaIdx = 0;  $rootScope.uuIdx = 0;
-  $rootScope.vm = { usrnm: '', cryptkey:'', eAES: '', dAES: '', aat123: 0, aatCrypt: '', loginOk: false,  accts:[{acctnm: '', pswd: '', xtra: ''}] };
-  $rootScope.encAES = { acctnm: '', pswd: '', xtra: ''};
-  $rootScope.decAES = { acctnm: '', pswd: '', xtra: ''};
- 
-  $rootScope.ngrUU = dbSvc.ngrUtil('{"utilkey":"crypto"}').query(); 
+  $rootScope.userRec = {
+    _id: '',
+    utilkey: 'crypto',
+    jsonData: { usrnm: '', accts: [] },
+  };
+  $rootScope.ngrAA = [];
+  $rootScope.ngrUU = [];
+  $rootScope.aaIdx = 0;
+  $rootScope.uuIdx = 0;
+  $rootScope.vm = {
+    usrnm: '',
+    cryptkey: '',
+    eAES: '',
+    dAES: '',
+    aat123: 0,
+    aatCrypt: '',
+    loginOk: false,
+    signup: false,
+    inSession: false,
+    accts: [{ acctnm: '', pswd: '', xtra: '' }],
+  };
+  $rootScope.encAES = { acctnm: '', pswd: '', xtra: '' };
+  $rootScope.decAES = { acctnm: '', pswd: '', xtra: '' };
+
+  $rootScope.ngrUU = dbSvc.ngrUtil('{"utilkey":"crypto"}').query();
   $rootScope.ngrAA = dbSvc.ngrapp01('{"appkey":"crypto"}').query();
-//  $rootScope.ngrAA.$promise.then(function(data) {
-//    $rootScope.gwflRec.recID  = $rootScope.ngrAA[0]._id; 
-//    angular.copy($rootScope.ngrAA[0].jsData.users, $rootScope.gwflRec.users); 
-//  });
-
-/*  // ES5
-var add1 = function(a, b) {
-  return a + b;
-};   console.log("add1:: ", add1(3, 40));
-// ES2015
-const add2 = (a, b) => a + b;  console.log("add2:: ", add2(3, 45));
-*/
-/*
-  const deleteObj = (data, column, search) => {
-    let resultObj = data.filter(m => m[column] == search);
-    return resultObj;  
-    };
-  $rootScope.deleted = deleteObj($rootScope.gwflRec.users, 'usrnm', $rootScope.vm.usrnm.toLowerCase());
-//  console.log(' ::deleted:: ', $rootScope.deleted);
-*/
-
 });
 
 ionApp.controller('MainCtrl', function(
-  $rootScope,$scope,$location,$filter,$timeout,$resource,
-  $ionicSideMenuDelegate,$ionicModal,$ionicPopup,
-  dbSvc,$crypto) {
+  $rootScope,
+  $scope,
+  $location,
+  $filter,
+  $timeout,
+  $resource,
+  $state,
+  $ionicSideMenuDelegate,
+  $ionicModal,
+  $ionicPopup,
+  dbSvc,
+  $crypto
+) {
+
+  if (! $rootScope.vm.loginOk) {
+   console.log('state.go> login');
+    $state.go('app.login');
+  };
 
   $scope.checkLogin = function() {
-    var jj = 0; var ii = 0;
+    if ($rootScope.vm.cryptkey == '' || $rootScope.vm.usrnm == '') {
+      alert('You must enter a username and a crypt key!');
+    } else {
+      $scope.userMatch();
+      if ($rootScope.vm.loginOk) {
+        $rootScope.vm.signup = false;
+        $rootScope.vm.inSession = true;
+      } else {
+        if ($rootScope.vm.signup) {
+          $scope.addUsername();
+          $rootScope.vm.loginOk = true;
+          $rootScope.vm.inSession = true;
+        } else {
+          alert(
+            'Unkown Username - Click OK to SignUp  >> ' + $rootScope.vm.usrnm
+          );
+          $rootScope.vm.signup = true;
+          $rootScope.vm.loginOk = true;
+          $rootScope.vm.inSession = false;
+        }
+      }
+    }
+  }; // end checkLogin
+
+  $scope.userMatch = function() {
+    var jj = 0;
+    var ii = 0;
     $rootScope.vm.loginOk = false;
     $rootScope.vm.usrnm = $rootScope.vm.usrnm.toLowerCase();
-  //    console.log('rS.vm::', $rootScope.vm);
-    angular.forEach($rootScope.ngrUU, function(obj) {      
-      if ( !($rootScope.vm.loginOk)  &&  
-      obj.jsonData.usrnm.toLowerCase() == $rootScope.vm.usrnm ) {
+
+    angular.forEach($rootScope.ngrUU, function(uuObj) {
+      if (
+        !$rootScope.vm.loginOk &&
+        uuObj.jsonData.usrnm.toLowerCase() == $rootScope.vm.usrnm
+      ) {
         $rootScope.vm.loginOk = true;
         $rootScope.uuIdx = jj;
-        angular.copy(obj.jsonData.accts,$rootScope.vm.accts);
-  //    console.log('copy::', $rootScope.uuIdx, '::', $rootScope.vm);
+        $rootScope.userRec.jsonData.usrnm = $rootScope.vm.usrnm;
+        $rootScope.userRec._id = $rootScope.ngrUU[jj]._id;
+        $rootScope.userRec.utilkey = $rootScope.ngrUU[jj].utilkey;
+        angular.copy($rootScope.ngrUU[jj].jsonData.accts, $rootScope.vm.accts);
+
         ii = 0;
-        for (ii = 0; ii < $rootScope.vm.accts.length; ii++) {        
-          $rootScope.vm.accts[ii].acctnm = 
-          $crypto.decrypt($rootScope.vm.accts[ii].acctnm, $rootScope.vm.cryptkey,256);
-        };
-  //      console.log('decVMaccts::',$rootScope.vm.accts);
-      };
+        for (ii = 0; ii < $rootScope.vm.accts.length; ii++) {
+          $rootScope.vm.accts[ii].acctnm = $crypto.decrypt(
+            $rootScope.vm.accts[ii].acctnm,
+            $rootScope.vm.cryptkey,
+            256
+          );
+        }
+      }
       jj++;
-    }); 
-    if ( !($rootScope.vm.loginOk) ) {
-      alert('Unkown Username.  Please try again using  username: "sample"');
-      $rootScope.vm.usrnm = 'sample';
-      $rootScope.vm.cryptkey = 'alpha1234';
-    }
-  };     // end checkLogin
+    });
+  }; // end userMatch
+
+  $scope.addUsername = function() {
+    var vSU = dbSvc.ngrUtil();
+    var vsu1 = new vSU();
+    $rootScope.userRec.jsonData.usrnm = $rootScope.vm.usrnm;
+    vsu1.utilkey = 'crypto';
+    vsu1.usrnmx = $rootScope.vm.usrnm;
+    vsu1.jsonData = JSON.stringify($rootScope.userRec.jsonData);
+    vsu1.$save(function() {
+      var qq = '{"usrnmx":"' + $rootScope.vm.usrnm + '"}';
+      $rootScope.ngrUU = dbSvc.ngrUtil(qq).query();
+      $rootScope.ngrUU.$promise.then(function(data) {
+  console.log('ngrUU::', $rootScope.ngrUU);
+        $scope.userMatch();
+      });
+    });
+    $rootScope.vm.accts = [];
+    $state.go('app.main');
+  }; // end addUsername
 
   $scope.aaView = function(aa) {
     $rootScope.aaIdx = aa;
     $rootScope.decAES.acctnm = $rootScope.vm.accts[aa].acctnm;
-    $rootScope.decAES.pswd = $crypto.decrypt($rootScope.vm.accts[$rootScope.aaIdx].pswd, $rootScope.vm.cryptkey,256);
-    $rootScope.decAES.xtra = $crypto.decrypt($rootScope.vm.accts[$rootScope.aaIdx].xtra, $rootScope.vm.cryptkey,256);
+    $rootScope.decAES.pswd = $crypto.decrypt(
+      $rootScope.vm.accts[$rootScope.aaIdx].pswd,
+      $rootScope.vm.cryptkey,
+      256
+    );
+    $rootScope.decAES.xtra = $crypto.decrypt(
+      $rootScope.vm.accts[$rootScope.aaIdx].xtra,
+      $rootScope.vm.cryptkey,
+      256
+    );
 
-    $location.path('/event/details');
+    $location.path('/app/details');
   };
 
   $scope.encAES = function() {
     //  $rootScope.encAES.acctnm = $crypto.encrypt($rootScope.decAES.acctnm, $rootScope.vm.cryptkey,256);
-    $rootScope.encAES.pswd = $crypto.encrypt($rootScope.decAES.pswd, $rootScope.vm.cryptkey,256);
-    $rootScope.encAES.xtra = $crypto.encrypt($rootScope.decAES.xtra, $rootScope.vm.cryptkey,256);
+    $rootScope.encAES.pswd = $crypto.encrypt(
+      $rootScope.decAES.pswd,
+      $rootScope.vm.cryptkey,
+      256
+    );
+    $rootScope.encAES.xtra = $crypto.encrypt(
+      $rootScope.decAES.xtra,
+      $rootScope.vm.cryptkey,
+      256
+    );
 
     $rootScope.vm.accts[$rootScope.aaIdx].pswd = $rootScope.encAES.pswd;
     $rootScope.vm.accts[$rootScope.aaIdx].xtra = $rootScope.encAES.xtra;
 
-    // $rootScope.gwflRec.users[$rootScope.uuIdx] = ;
-    $rootScope.gwflRec.users[$rootScope.uuIdx].accts[$rootScope.aaIdx].pswd = 
-      $rootScope.encAES.pswd;
-    $rootScope.gwflRec.users[$rootScope.uuIdx].accts[$rootScope.aaIdx].xtra = 
-      $rootScope.encAES.xtra;
+    $rootScope.userRec.accts[$rootScope.aaIdx].pswd = $rootScope.encAES.pswd;
+    $rootScope.userRec.accts[$rootScope.aaIdx].xtra = $rootScope.encAES.xtra;
   };
 
-$scope.saveRec = function() {
-dbSvc.ngrUtil().get({id: '5ed997272d9d125d00016725'}).$promise.then(function(urec) {
-  urec.usrnm = 'samplex';
-  urec.$save();
-}); 
-};   //  $scope.saveRec();
+  $scope.saveRec = function() {
+    dbSvc
+      .ngrUtil()
+      .get({ id: '5ed997272d9d125d00016725' })
+      .$promise.then(function(urec) {
+        urec.usrnm = 'samplex';
+        urec.$save();
+      });
+  }; //  $scope.saveRec();
 
   $scope.newRec = function() {
     $scope.encAES();
-    console.log('$vm.user::', $rootScope.aaIdx, " :: ",  $rootScope.encAES);
+    console.log('$vm.user::', $rootScope.aaIdx, ' :: ', $rootScope.encAES);
     var vnrUtil = dbSvc.ngrUtil();
     var newRec1 = new vnrUtil();
-    $rootScope.newRecID = newRec1;
     newRec1.utilkey = 'crypto';
-    newRec1.jsonData = JSON.stringify($rootScope.gwflRec.users[$rootScope.uuIdx]);
+    newRec1.jsonData = JSON.stringify($rootScope.userRec.jsonData);
     // console.log('$save', newRec1);
     newRec1.$save();
   };
-
 });
 
 // DB Service
@@ -223,12 +295,46 @@ ionApp.factory('dbSvc', function($rootScope, $http, $resource) {
 
   return {
     getMeta: _getMeta,
-    ngrUtil: _ngrUtil, 
-    ngrapp01: _ngrapp01 
+    ngrUtil: _ngrUtil,
+    ngrapp01: _ngrapp01,
   };
-
 });
 
 ionApp.factory('aatCrypt', function() {
   return '123a';
 });
+
+//  $rootScope.ngrAA.$promise.then(function(data) { $scope.userMatch();
+//    $rootScope.userRec.recID  = $rootScope.ngrAA[0]._id;
+//    angular.copy($rootScope.ngrAA[0].jsData.users, $rootScope.userRec);
+//  });
+
+/*  // ES5
+var add1 = function(a, b) {
+  return a + b;
+};   console.log("add1:: ", add1(3, 40));
+// ES2015
+const add2 = (a, b) => a + b;  console.log("add2:: ", add2(3, 45));
+*/
+/*
+  const deleteObj = (data, column, search) => {
+    let resultObj = data.filter(m => m[column] == search);
+    return resultObj;  
+    };
+  $rootScope.deleted = deleteObj($rootScope.userRec, 'usrnm', $rootScope.vm.usrnm.toLowerCase());
+//  console.log(' ::deleted:: ', $rootScope.deleted);
+
+
+nameApp.controller('ListCtrl', function($scope, $state) {
+  $scope.changePage = function(){
+    $state.go('view', {movieid: 1});
+  }    
+});
+ 
+nameApp.controller('ViewCtrl', function($scope, $stateParams, $ionicHistory) {
+  console.log($stateParams.movieid);
+  $scope.goBack = function(){
+    $ionicHistory.goBack();
+  }    
+});
+*/
